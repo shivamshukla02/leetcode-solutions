83739c74bfa0899c5b5340df1fd63f326e4159af
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-08-14 12:23:36 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.36%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+       while (n != 1 && n != 4) {
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                int i = digit * digit;
+                sum += i;
+                n = n / 10;
+            }
+            n = sum;}
+        if (n == 1) {
+            return true;}
+        return false;}
+};
