eb65f471e44e6feb45cd4a7d2e68bd4e25e65fb6
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-09-07 10:51:34 +0530

    Time: 3 ms (12.69%), Space: 42.5 MB (71.46%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int n) {
+        int c=0;
+     int   temp=n;
+        while(temp>0){
+            temp=temp/10;
+            c++;}
+            int a[] = new int[c];
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i<c;i++){
+                int d=n%10;
+                a[i]=d;
+                n=n/10;}
+                Arrays.sort(a);
+                int p = a[a.length-1]*a[a.length-2];
+                return p;
+            }
+}
